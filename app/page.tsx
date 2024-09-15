94c0fcfbc2e23363fc16582d8e40cac3102a26dd
axadjon18 <mannonovaxadjon18@gmail.com>
2024-09-15 12:03:47 +0500

    ++

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-5xl font-bold animate-bounce opacity-50 mt-5 text-center">Axadjon Mannonov</h1>
+      <h2 className="text-3xl font-bold text-center">Software Engineer</h2>
     </div>
 
   );
